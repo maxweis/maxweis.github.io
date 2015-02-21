c51c8ea16dcd83c06882dbbc03563695fbf7dff2
Maxon Weis <maxrobweis@gmail.com>
2015-02-20 22:42:09 -0600

    js trial

---
+++ header.js
@@ -0,0 +1,31 @@
+var page=window.location.pathname;
+alert(page);
+//if (page == "/index.html"){
+        //document.write('\
+        //<a class="btn" href="index.html"><button class="btn" type="submit"><font color=blue>Home</font></button></a>\
+        //');
+//}
+document.write('\
+<style>\
+a:link{color:#3366FF;}\
+a:visited{color:#9C9D9C;}\
+a:hover{color:#5C85FF;}\
+</style>\
+\
+<link rel="shortcut icon" href="images/mw.ico" />\
+<body link="#3366FF" background="images/tile.jpg">\
+\
+<center>\
+<a class="btn" href="art_home.html"><button class="btn" type="submit">Art</button></a>\
+<a class="btn" href="downloads_home.html"><button class="btn" type="submit">Downloads</button></a>\
+<a class="btn" href="essays_home.html"><button class="btn" type="submit">Essays</button></a>\
+<a class="btn" href="humor_home.html"><button class="btn" type="submit">Humor</button></a>\
+<a class="btn" href="links_home.html"><button class="btn" type="submit">Links</button></a>\
+<a class="btn" href="videos_home.html"><button class="btn" type="submit">Videos</button></a>\
+</center>\
+<font color="white" face="ubuntu">\
+\
+<h1 style="text-align:center;">The Max Weis Page of Various Nonsense</h1>\
+</font>\
+<font color="white" face="Droid Sans">\
+        ');
