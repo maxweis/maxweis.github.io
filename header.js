7f640e69f4277af075b7bc1a5ad8566c5a7f87d4
Maxon Weis <maxrobweis@gmail.com>
2015-02-27 02:33:50 -0600

    update

--- header.js
+++ header.js
@@ -1,10 +1,3 @@
-var page=window.location.pathname;
-alert(page);
-//if (page == "/index.html"){
-        //document.write('\
-        //<a class="btn" href="index.html"><button class="btn" type="submit"><font color=blue>Home</font></button></a>\
-        //');
-//}
 document.write('\
 <style>\
 a:link{color:#3366FF;}\
@@ -14,15 +7,75 @@ a:hover{color:#5C85FF;}\
 \
 <link rel="shortcut icon" href="images/mw.ico" />\
 <body link="#3366FF" background="images/tile.jpg">\
-\
-<center>\
-<a class="btn" href="art_home.html"><button class="btn" type="submit">Art</button></a>\
-<a class="btn" href="downloads_home.html"><button class="btn" type="submit">Downloads</button></a>\
-<a class="btn" href="essays_home.html"><button class="btn" type="submit">Essays</button></a>\
-<a class="btn" href="humor_home.html"><button class="btn" type="submit">Humor</button></a>\
-<a class="btn" href="links_home.html"><button class="btn" type="submit">Links</button></a>\
-<a class="btn" href="videos_home.html"><button class="btn" type="submit">Videos</button></a>\
-</center>\
+');
+
+var page=window.location.pathname;
+if (page == "/art_home.html"){
+        document.write('\
+        <a class="btn" href="art_home.html"><button class="btn" type="submit"><font color=blue>Art</font></button></a>\
+        ');
+}
+else{
+        document.write('\
+        <a class="btn" href="art_home.html"><button class="btn" type="submit">Art</button></a>\
+        ');
+}
+
+if (page == "/downloads_home.html"){
+        document.write('\
+        <a class="btn" href="downloads_home.html"><button class="btn" type="submit"><font color = blue>Downloads</font></button></a>\
+        ');
+}
+else{
+        document.write('\
+        <a class="btn" href="downloads_home.html"><button class="btn" type="submit">Downloads</button></a>\
+        ');
+}
+
+if (page == "/essays_home.html"){
+        document.write('\
+        <a class="btn" href="essays_home.html"><button class="btn" type="submit"><font color = blue>Essays</font></button></a>\
+        ');
+}
+else{
+        document.write('\
+        <a class="btn" href="essays_home.html"><button class="btn" type="submit">Essays</button></a>\
+        ');
+}
+
+if (page == "/humor_home.html"){
+        document.write('\
+        <a class="btn" href="humor_home.html"><button class="btn" type="submit"><font color = blue>Humor</font></button></a>\
+        ');
+}
+else{
+        document.write('\
+        <a class="btn" href="humor_home.html"><button class="btn" type="submit">Humor</button></a>\
+        ');
+}
+
+if (page == "/links_home.html"){
+        document.write('\
+        <a class="btn" href="links_home.html"><button class="btn" type="submit"><font color = blue>Links</font></button></a>\
+        ');
+}
+else{
+        document.write('\
+        <a class="btn" href="links_home.html"><button class="btn" type="submit">Links</button></a>\
+        ');
+}
+
+if (page == "videos_home.html"){
+        document.write('\
+        <a class="btn" href="videos_home.html"><button class="btn" type="submit"><font color = blue>Videos</font></button></a>\
+        ');
+}
+else{
+        document.write('\
+        <a class="btn" href="videos_home.html"><button class="btn" type="submit">Videos</button></a>\
+        ');
+}
+document.write('
 <font color="white" face="ubuntu">\
 \
 <h1 style="text-align:center;">The Max Weis Page of Various Nonsense</h1>\
